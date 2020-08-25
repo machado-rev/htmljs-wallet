1f538715208a52da7e375979ba3c5b7efd1405d1
Leandro Machado <sr.machado@gmail.com>
2020-08-25 14:50:07 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -15,9 +15,9 @@ module.exports = {
     extensions: ['.ts', '.js']
   },
   output: {
-    filename: 'qtum-wallet.js',
+    filename: 'htmlcoin-wallet.js',
     path: path.resolve(__dirname, 'dist'),
-    library: "QtumWallet",
+    library: "HtmlWallet",
     libraryTarget: "umd",
   }
 }
